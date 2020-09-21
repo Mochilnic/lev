173685f8500974a64d1aff411079abfa7f87b8b7
Lev <mr.svitelskiy@gmail.com>
2020-09-21 22:21:42 +0300

    +changes

--- src/main.java
+++ src/main.java
@@ -1,7 +1,8 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Bye");
+        System.out.println("Suren");
+        System.out.println("Shawarma");
+        int a=69;
 
     }
 }
