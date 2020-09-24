e3a02204a6bfcecaf1eafab056dd24b13c8822c9
Lev <mr.svitelskiy@gmail.com>
2020-09-24 09:11:54 +0300

    new

--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World and Suren");
+        System.out.println("Hello World and hold my beer");
+        System.out.println("Suren");
     }
 }
