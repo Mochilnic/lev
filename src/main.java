3074faecea394e456793d0c186ec9509f3f83445
LevSv <mr.svitelskiy@gmail.com>
2021-09-23 01:32:11 +0300

    changed name

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Suren");
         System.out.println("Shawarma");
-        int a=69;
+        int a=1488;
 
     }
 }
